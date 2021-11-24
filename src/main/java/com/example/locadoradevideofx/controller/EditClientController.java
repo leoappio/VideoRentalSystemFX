069d75291786cb7ae2034a42997d2d32d2e11860
Leonardo <leonardo.appio@gmail.com>
2021-11-24 15:08:53 -0300

    edição de cliente concluida

--- src/main/java/com/example/locadoradevideofx/controller/EditClientController.java
+++ src/main/java/com/example/locadoradevideofx/controller/EditClientController.java
@@ -7,6 +7,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 import java.io.IOException;
 import java.sql.SQLException;
@@ -17,6 +18,10 @@ public class EditClientController {
     private Label successMessage;
     @FXML
     private ChoiceBox clientsList;
+    @FXML
+    private TextField nameField;
+    @FXML
+    private TextField phoneField;
 
     private List<Client> clients;
 
@@ -28,14 +33,39 @@ public class EditClientController {
     public void initialize() throws SQLException {
         attClientList();
 
+        clientsList.setOnAction((event) -> {
+            int selectedIndex = clientsList.getSelectionModel().getSelectedIndex();
+            if(selectedIndex != -1) {
+                nameField.setText(clients.get(selectedIndex).name);
+                phoneField.setText(clients.get(selectedIndex).phone);
+            }
+        });
     }
 
     private void attClientList() throws SQLException {
         clients = Database.getAllClients();
-
         clientsList.getItems().clear();
+
         for(Client client : clients){
             clientsList.getItems().add(client.id+" - "+client.name);
         }
+    }
+
+    public void onEditClickButton(ActionEvent e) throws SQLException {
+        if(nameField.getText()!="" && phoneField.getText()!= ""){
+            int selectedIndex = clientsList.getSelectionModel().getSelectedIndex();
+            Client clientToEdit = clients.get(selectedIndex);
+            clientToEdit.setName(nameField.getText());
+            clientToEdit.setPhone(phoneField.getText());
+            Database.updateClient(clientToEdit);
+            successMessage.setText("Cliente editado com sucesso!");
+            attClientList();
+            nameField.setText("");
+            phoneField.setText("");
+        }else{
+            successMessage.setText("Os dados não podem estar vazios");
+        }
+
+
     }
 }
